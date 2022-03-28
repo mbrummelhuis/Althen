36ec9102aa5afcdf613945a39b524a892b87402f
Martijn Brummelhuis <mbrummelhuis@gmail.com>
2022-03-28 11:52:53 +0200

    Analysis 21-3 temptest Interay

--- Temperature compensation optimisation/Interay/blockbaxAnalyser.py
+++ Temperature compensation optimisation/Interay/blockbaxAnalyser.py
@@ -5,15 +5,22 @@ from scipy.stats import linregress
 import numpy as np
 
 class blockbaxAnalyser():
-    def __init__(self, sb_numbers):
+    def __init__(self, sb_numbers, plot_from_date, plot_till_date):
         self.sb_numbers = sb_numbers
         self.dfs = []
         self.colours = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'cornlowerblue', 'lightcoral', 'chocolate', 'slateblue', 'orange', 'greenyellow']
-        self.y_meas = []
+        self.y_means = []
         self.y_sigmas = []
+
+        self.plot_from_date = plot_from_date
+        self.plot_till_date = plot_till_date
 
         self.slope = None
         self.intercept = None
+
+        # Trendline settings (min and max temperature)
+        self.tl_min = -20
+        self.tl_max =  50
 
     def loadData(self):
         for i in range(len(self.sb_numbers)):
@@ -30,21 +37,42 @@ class blockbaxAnalyser():
             temp_df['Value'] = pd.to_numeric(temp_df['Value'], downcast="float")
             y_df['Value'] = pd.to_numeric(y_df['Value'], downcast="float")
 
-            self.y_meas.append(y_df["Value"].mean())
-            self.y_sigmas.append(y_df["Value"].std())
+            # Delete before and after
+            data = [temp_df["Date"], temp_df["Value"], y_df["Value"]]
+            headers = ['Datetime', 'Temperature', 'Y value']
+            data = pd.concat(data, axis=1, keys=headers)
 
-            data = [temp_df["Date"], temp_df["Value"], y_df["Value"], (y_df["Value"]-self.y_meas[i]), (y_df["Value"]-self.y_meas[i])/self.y_sigmas[i]]
-            headers = ['Datetime', 'Temperature', 'Y value', 'Y value (half norm)', 'Y value (norm)']
+            data["Datetime"] = pd.to_datetime(data["Datetime"], format = "%Y-%m-%d %H:%M:%S")
+            data.sort_index(axis=0, ascending=False)
+            data = data.set_index("Datetime", drop=False)
+            delete_before = pd.Timestamp(self.plot_from_date) # Year, month, date
+            delete_after = pd.Timestamp(self.plot_till_date)
+            data = data.loc[(data.index > delete_before)]
+            data = data.loc[(data.index < delete_after)]
 
-            self.dfs.append(pd.concat(data, axis=1, keys=headers))
+            # Calculate means and stdevs            
+            self.y_means.append(data["Y value"].mean())
+            self.y_sigmas.append(data["Y value"].std())
+
+            # Construct extended dataset
+            ext_data = [data["Datetime"], data["Temperature"], data["Y value"], (data["Y value"]-self.y_means[i]), (data["Y value"]-self.y_means[i])/self.y_sigmas[i]]
+            ext_headers = ['Datetime', 'Temperature', 'Y value', 'Y value (half norm)', 'Y value (norm)']
+
+            # Append dataframe to object attribute
+            self.dfs.append(pd.concat(ext_data, axis=1, keys=ext_headers))
 
     
     def plotTempAngle(self):
+        """
+        Plots temperature on x-axis and angle on y-axis of all smartbricks.
+        If trendline has been calculated, will plot trendline
+        """
         for i in range(len(self.sb_numbers)):
             plt.scatter(self.dfs[i]['Temperature'], self.dfs[i]['Y value'], label=str(self.sb_numbers[i]))
 
+        # Plot trendline
         if self.slope != None:
-            x = np.linspace(0,25,1000)
+            x = np.linspace(self.tl_min,self.tl_max,1000)
             y = self.slope * x + self.intercept
             plt.plot(x,y)
         
@@ -55,11 +83,15 @@ class blockbaxAnalyser():
         plt.show()
 
     def plotTempHalfNormAngle(self):
+        """
+        Plots temperature on x-axis and half normalised (w.r.t. mean) angle on y-axis of all smartbricks.
+        If trendline has been calculated, will plot trendline        
+        """
         for i in range(len(self.sb_numbers)):
             plt.scatter(self.dfs[i]['Temperature'], self.dfs[i]['Y value (half norm)'], label=str(self.sb_numbers[i]))
         
         if self.slope != None:
-            x = np.linspace(0,20,1000)
+            x = np.linspace(self.tl_min,self.tl_max,1000)
             y = self.slope * x + self.intercept
             plt.plot(x,y)
         
@@ -70,11 +102,16 @@ class blockbaxAnalyser():
         plt.show()
 
     def plotTempNormAngle(self):
+        """
+        Plots temperature on x-axis and fully normalised (w.r.t. mean and stdev) angle on y-axis of all smartbricks.
+        If trendline has been calculated, will plot trendline           
+        """
         for i in range(len(self.sb_numbers)):
             plt.scatter(self.dfs[i]['Temperature'], self.dfs[i]['Y value (norm)'], label=str(self.sb_numbers[i]))
         
+        # Plot trendline
         if self.slope != None:
-            x = np.linspace(0,20,1000)
+            x = np.linspace(self.tl_min,self.tl_max,1000)
             y = self.slope * x + self.intercept
             plt.plot(x,y)
         
@@ -85,6 +122,9 @@ class blockbaxAnalyser():
         plt.show()
     
     def trendline(self):
+        """
+        Calculates the slope, intercept and r-squared for a trendline of a set.
+        """
         templist = []
         ylist = []
         # Append all temperature and y data in separate lists
@@ -100,6 +140,9 @@ class blockbaxAnalyser():
         print("R-squared: %f" % r_value**2)
 
     def trendlineHalfNorm(self):
+        """
+        Calculates the slope, intercept and r-squared for a trendline of a set with angles normalised w.r.t. mean.
+        """
         templist = []
         ylist = []
         # Append all temperature and y data in separate lists
@@ -115,6 +158,9 @@ class blockbaxAnalyser():
         print("R-squared: %f" % r_value**2)
 
     def trendlineNorm(self):
+        """
+        Calculates the slope, intercept and r-squared for a trendline of a set with angles normalised w.r.t. mean and stdev.
+        """
         templist = []
         ylist = []
         # Append all temperature and y data in separate lists
@@ -131,14 +177,24 @@ class blockbaxAnalyser():
 
 if __name__=="__main__":
     #sb_numbers = [141419, 141422, 141425, 141427, 141430, 141431, 141433]
-    sbs = [148040, 148054, 148062, 148068, 148070, 148074, 148077, 148084, 148089, 148094, 148101, 148102]
+    #sbs = [148040, 148054, 148062, 148068, 148070, 148074, 148077, 148084, 148089, 148094, 148101, 148102]
+
+    plot_from_date = "2022-03-21 18:00:00" # put in date string with format YYYY-MM-DD HH:MM:SS
+    plot_till_date = "2022-03-24 01:30:00"
+
+    sbs = [148088, 148097, 148098, 148099, 148105, 148106, 148107]
+
+    analyser = blockbaxAnalyser(sbs, plot_from_date, plot_till_date)
+    analyser.loadData()
+    analyser.plotTempNormAngle()
+
 
     for sb in sbs:    
-        analyser = blockbaxAnalyser([sb])
+        analyser = blockbaxAnalyser([sb], plot_from_date, plot_till_date)
         analyser.loadData()
         print("-------------------"+str(sb))
-        print(analyser.y_meas)
-        print(analyser.y_sigmas)
+        print("Means: ", analyser.y_means)
+        print("Stdev: ", analyser.y_sigmas)
 
         #print("Not normalised --------------------------")
         #analyser.trendline()
